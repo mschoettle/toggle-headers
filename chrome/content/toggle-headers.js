0c649341c81a0b1c09c64088634741206fc04e6c
Matthias Schoettle <git@mattsch.com>
2012-01-26 20:31:00 -0500

    Adds first version of Toggle Headers.

---
+++ chrome/content/toggle-headers.js
@@ -0,0 +1,12 @@
+function toggleMailHeaders() {
+    
+    var currentHeaderSetting = gPrefBranch.getIntPref("mail.show_headers");
+    
+    // normal headers
+    if (currentHeaderSetting == 1)
+        goDoCommand('cmd_viewAllHeader');
+    // all headers
+    else if (currentHeaderSetting == 2)
+        goDoCommand('cmd_viewNormalHeader');
+    
+}
