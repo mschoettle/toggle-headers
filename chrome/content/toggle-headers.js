9d14d43118151a49c59fe1dc396d75369c9871a3
Matthias Schoettle <git@mattsch.com>
2015-10-28 22:07:39 -0400

    Adds support for CompactHeader 2.0.9 and lower, and the new structure of 2.1.0.
    Internally, the structure was adjusted, e.g., it is now from org_mozdev_compactHeader (was org.mozdev.compactHeader).
    The function of the used version is used accordingly.

--- chrome/content/toggle-headers.js
+++ chrome/content/toggle-headers.js
@@ -28,32 +28,51 @@ com_mattsch_toggleHeaders.toggleHeadersView = function() {
 // (this solution only works when Thunderbird is not started with all headers shown)
 com_mattsch_toggleHeaders.handleCompactHeader = function(oldHeaderSetting) {
     // Check if CompactHeaders is installed...
-    var compactHeaders = document.getElementById('CompactHeader_collapsedHeaderView');
+    var compactHeaders = com_mattsch_toggleHeaders.getCompactHeaders();
     
     if (compactHeaders != null) {
+        var compactHeadersView = document.getElementById('CompactHeader_collapsedHeaderView');
+        
         // Somehow collapsed means the opposite:
         // collapsed = true: expanded
         // collapsed = false: collapsed
-        if (!compactHeaders.collapsed) {
+        if (!compactHeadersView.collapsed) {
             // If normal headers were enabled before, just expand the headers view...
             if (oldHeaderSetting == 1) {
-                org.mozdev.compactHeader.pane.coheToggleHeaderView();
+                compactHeaders.pane.coheToggleHeaderView();
                 com_mattsch_toggleHeaders.compactHeadersWasCollapsed = true;
             }
             // If the headers view was collapsed and all headers enabled,
             // normal headers will be shown now which would be wrong behavior.
             // Just show expand the headers view instead.
             else if (oldHeaderSetting == 2) {
-                org.mozdev.compactHeader.pane.coheToggleHeaderView();
+                compactHeaders.pane.coheToggleHeaderView();
                 goDoCommand('cmd_viewAllHeader');
                 com_mattsch_toggleHeaders.compactHeadersWasCollapsed = true;
             }
         }
         // Header view is expanded, but was previously collapsed.
         // This means we need to collapse it back again.
-        else if (compactHeaders.collapsed && com_mattsch_toggleHeaders.compactHeadersWasCollapsed) {
-            org.mozdev.compactHeader.pane.coheToggleHeaderView();
+        else if (compactHeadersView.collapsed && com_mattsch_toggleHeaders.compactHeadersWasCollapsed) {
+            compactHeaders.pane.coheToggleHeaderView();            
             com_mattsch_toggleHeaders.compactHeadersWasCollapsed = false;
         }    
     }
+}
+
+/**
+ * Returns the reference to CompactHeaders supporting both CompactHeaders <= 2.9.0 and higher.
+ */
+com_mattsch_toggleHeaders.getCompactHeaders = function() {
+    // Check for CompactHeaders v2.0.9 and lower.
+    if (typeof org !== 'undefined' && typeof org.mozdev !== 'undefined'
+          && typeof org.mozdev.compactHeader !== 'undefined') {
+              return org.mozdev.compactHeader;
+    } 
+    // Check for CompactHeaders v2.1.0 and higher.
+    else if (typeof org_mozdev_compactHeader !== 'undefined') {
+        return org_mozdev_compactHeader;
+    }
+    
+    return null;
 }
