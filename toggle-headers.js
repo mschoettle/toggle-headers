f46846ba9095f6a5617adca293664e3b0ecfb67a
Matthias Schoettle <git@mattsch.com>
2020-11-09 17:13:09 -0500

    Replaces legacy code with actual MailExtension including experiment to be able to access preferences.

---
+++ toggle-headers.js
@@ -0,0 +1,31 @@
+var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+var toggleHeadersApi = class extends ExtensionCommon.ExtensionAPI {
+  getAPI(context) {
+    return {
+      toggleHeadersApi: {
+        async toggleHeaders() {
+          let window = Services.wm.getMostRecentWindow("mail:3pane");
+
+           // Get the preferences mail branch...
+      	  let mailPrefs = Components
+              .classes["@mozilla.org/preferences-service;1"]
+              .getService(Components.interfaces.nsIPrefService)
+              .getBranch("mail.");
+
+          let currentHeaderSetting = mailPrefs.getIntPref("show_headers");
+          
+          // Switch from normal to all headers.
+          if (currentHeaderSetting == 1) {
+            window.goDoCommand('cmd_viewAllHeader');
+          }
+          // Switch from all to normal headers.
+          else if (currentHeaderSetting == 2) {
+            window.goDoCommand('cmd_viewNormalHeader');
+          }
+        },
+      }
+    }
+  }
+};
