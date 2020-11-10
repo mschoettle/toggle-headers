ccaf1ba14368b552e463a3925ad97b89ee936977
Matthias Schoettle <git@mattsch.com>
2020-11-09 20:59:39 -0500

    Reloads messages in all windows.

--- toggle-headers.js
+++ toggle-headers.js
@@ -8,14 +8,14 @@ var toggleHeadersApi = class extends ExtensionCommon.ExtensionAPI {
         async toggleHeaders() {
           let window = Services.wm.getMostRecentWindow("mail:3pane");
 
-           // Get the preferences mail branch...
+          // Get the preferences mail branch...
       	  let mailPrefs = Components
               .classes["@mozilla.org/preferences-service;1"]
               .getService(Components.interfaces.nsIPrefService)
               .getBranch("mail.");
 
           let currentHeaderSetting = mailPrefs.getIntPref("show_headers");
-          
+
           // Switch from normal to all headers.
           if (currentHeaderSetting == 1) {
             window.goDoCommand('cmd_viewAllHeader');
@@ -23,6 +23,17 @@ var toggleHeadersApi = class extends ExtensionCommon.ExtensionAPI {
           // Switch from all to normal headers.
           else if (currentHeaderSetting == 2) {
             window.goDoCommand('cmd_viewNormalHeader');
+          }
+
+          // Get all current windows to reload the message in them.
+          let windows = Services.wm.getEnumerator(null)
+          while (windows.hasMoreElements()) {
+            let currentWindow = windows.getNext()
+            
+            // The main window has already been reloaded.
+            if (currentWindow != window) {
+              currentWindow.ReloadMessage();
+            }
           }
         },
       }
