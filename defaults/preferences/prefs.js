30c1d6e3d6e5206e62e561a819a786f13782bae7
Matthias Schoettle <git@mattsch.com>
2020-11-09 20:33:02 -0500

    Removes old artifacts and adds locales based on new format.

--- defaults/preferences/prefs.js
+++
@@ -1,2 +0,0 @@
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.mattsch-toggle-headers.description", "chrome://mattsch-toggle-headers/locale/toggle-headers.properties");
